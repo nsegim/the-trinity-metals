2712ac6d97cc3bc20dbd9cf787c35b8932d857a1
Moussa <nsengimanamoussa3@gmail.com>
2025-01-02 10:48:21 +0200

    using hasRouter on app.jsx to fix server error

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import SiteHeader from "./components/header/Header";
-import { BrowserRouter, Router, Route, Routes, HashRouter } from "react-router-dom";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import SiteHeader from "./components/header/Header";
+// import { BrowserRouter, Router, Route, Routes, HashRouter } from "react-router-dom";
+// import HomePage from "./components/Home/Home";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//       <hashRouter basename={import.meta.env.BASE_URL}>
+//         <Routes>
+//           <Route path="/" element={<HomePage />}></Route>
+//         </Routes>
+//       </hashRouter>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <BrowserRouter basename="/trinity-metals/">
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
