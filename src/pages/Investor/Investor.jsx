49753c342baa39471d4ecb5cdd063272ea63d99e
Moussa <nsengimanamoussa3@gmail.com>
2025-02-10 07:14:54 +0200

    Updates

--- src/pages/Investor/Investor.jsx
+++
@@ -1,9 +0,0 @@
-const InvestorPage = ()=> {
-    return(
-        <>
-          <h1>This is the Investor Page</h1>
-        </>
-    )
-}
-
-export default InvestorPage
--- src/pages/Project/Projects.jsx
+++
@@ -1,7 +0,0 @@
-const ProjectPage = () => {
-    return(
-        <h1>This is the Project Page</h1>
-    )
-}
-
-export default ProjectPage
