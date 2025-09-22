d0fe17ddace1670895f9c4c66aad297c77de4c64
Moussa <nsengimanamoussa3@gmail.com>
2025-09-22 16:46:26 +0200

    mid-final111 push

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 
 import App from './App.jsx'
+import i18n from './i18n/config.js'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap-icons/font/bootstrap-icons.css'
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css'
 createRoot(document.getElementById('root')).render(
   
